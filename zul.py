5f6824f6073ddb39cad23695c27649712b8122af
muhammad zulhanjani <151037196+mzulhanjani@users.noreply.github.com>
2023-11-28 23:40:39 +0800

    Add files via upload
    tugas mecari bilangan random

---
+++ zul.py
@@ -0,0 +1,14 @@
+import subprocess
+
+subprocess.run(["python", "random_1.py"])
+
+
+# Panggil program_urut_ganjil_genap.py sebagai subprocess
+result = subprocess.run(["python", "random_1.py"], capture_output=True, text=True)
+
+# Simpan output ke dalam file
+with open("hasil_random.txt", "w") as file:
+    file.write(result.stdout)
+
+# Tampilkan output di terminal
+print(result.stdout)
