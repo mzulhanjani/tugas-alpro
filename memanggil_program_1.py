e3c870dd84de1382b27bbcc22aded9c967ef4b2f
muhammad zulhanjani <151037196+mzulhanjani@users.noreply.github.com>
2023-11-29 11:17:04 +0800

    Add files via upload
    tugas 1 menentukan bilangan ganjil dan genap kemudian kalikan ke bilangan terdekat

---
+++ memanggil_program_1.py
@@ -0,0 +1,15 @@
+# memangil program random 1 dan menempilkan ke bentuk file 
+import subprocess
+
+subprocess.run(["python", "random_1.py"])
+
+
+# Panggil program_urut_ganjil_genap.py sebagai subprocess
+result = subprocess.run(["python", "random_1.py"], capture_output=True, text=True)
+
+# Simpan output ke dalam file
+with open("hasil_random.txt", "w") as file:
+    file.write(result.stdout)
+
+# Tampilkan output di terminal
+print(result.stdout)
